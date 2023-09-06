e01e679356686b125666519c45586db3db4214ef
Slavko Tepavcevic <slavko.tepavcevic@webservis.rs>
2023-09-06 14:48:58 +0200

    Remove the order field from checkout

--- wp-content/themes/generatepress_child/functions.php
+++ wp-content/themes/generatepress_child/functions.php
@@ -5,3 +5,8 @@
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
+
+/**
+ * Remove the order field from checkout.
+ */
+add_filter( 'woocommerce_enable_order_notes_field', '__return_false', 9999 );
